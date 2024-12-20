d0498f7e7efa50946ad4f6b65592fffc336cfabf
GustavoHBarth <barthgustavo5@gmail.com>
2024-12-20 15:29:48 -0300

    Adicionar arquivos de projeto.

---
+++ WebShop.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebShop.ProductApi.Models;
+
+namespace WebShop.ProductApi.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product>? Products { get; set; }
+    public DbSet<Category>? Categories { get; set; }
+}
+
