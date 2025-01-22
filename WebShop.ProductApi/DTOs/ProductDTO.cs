475e180f936fa41003fa630545ba63c373a959fa
GustavoHBarth <barthgustavo5@gmail.com>
2025-01-21 22:52:20 -0300

    Arrumando alguns erros em ProductDTO e CategoryDTO

--- WebShop.ProductApi/DTOs/ProductDTO.cs
+++ WebShop.ProductApi/DTOs/ProductDTO.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using WebShop.ProductApi.Models;
 
 namespace WebShop.ProductApi.DTOs;
@@ -24,7 +25,7 @@ public class ProductDTO
     [Range(1, 9999)]
     public long Stock { get; set; }
     public string? ImageURL { get; set; }
-
+    [JsonIgnore]
     public Category? Category { get; set; }
     public int CategoryId { get; set; }
 }
