dd6bec720c6b0116dbe6bef8a974d515ccab6ae9
GustavoHBarth <barthgustavo5@gmail.com>
2025-01-31 10:23:35 -0300

    Criando a aplicação para consumir o microsserviço

---
+++ WebShop.Web/Models/CategoryViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebShop.Web.Models;
+
+public class CategoryViewModel
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+}
---
+++ WebShop.Web/Services/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using WebShop.Web.Models;
+using WebShop.Web.Services.Contracts;
+
+namespace WebShop.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IHttpClientFactory clientFactory;
+        private const string apiEndpoint = "/api/products/";
+        private readonly JsonSerializerOptions _options;
+        private ProductViewModel productVM;
+        private IEnumerable<ProductViewModel> productVMs;
+        public ProductService(IHttpClientFactory clientFactory)
+        {
+            this.clientFactory = clientFactory;
+            _options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+        }
+
+        public Task<IEnumerable<ProductViewModel>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ProductViewModel> FindProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ProductViewModel> CreateProduct(ProductViewModel productVM)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ProductViewModel> UpdateProduct(ProductViewModel productVM)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> DeleteProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
